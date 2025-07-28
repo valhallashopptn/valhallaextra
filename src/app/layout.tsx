61e98114b5d4be2c0528ef045b38a9188c29651d
VALHALLA SHOP <valhallashopp0@gmail.com>
2025-07-28 02:17:26 +0000

    its not aboutch cashing browser cuz iam using private browser and still

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,17 @@ const inter = Inter({
 });
 
 export async function generateMetadata(): Promise<Metadata> {
-  const { siteTitle } = await getSettings(['siteTitle']);
+  const { siteTitle, faviconUrl } = await getSettings(['siteTitle', 'faviconUrl']);
   
+  const favicon = faviconUrl ? `${faviconUrl}&v=2` : '/favicon.ico';
+
   return {
     title: {
       default: siteTitle || 'ApexTop',
       template: `%s | ${siteTitle || 'ApexTop'}`,
     },
     description: 'Top up your favorite games.',
+    icons: [{ rel: 'icon', url: favicon }],
   };
 }
 
