6e30fe505d7c5011103ec6ca6ad80c156a85d1a9
VALHALLA SHOP <valhallashopp0@gmail.com>
2025-07-30 01:15:48 +0000

    <!--Start of Tawk.to Script--> <script type="text/javascript"> var Tawk_

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,7 +98,7 @@ export default async function RootLayout({
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/6885851b9462141926a5e799/1j14re5c6';
+            s1.src='https://embed.tawk.to/6885851b9462141926a5e799/default';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
